33d411e0ff831da3a4fbadd67c004686568663f4
Nyan Dân <rrelita56@gmail.com>
2020-06-17 18:05:18 +0700

    Update users.js

--- src/server/routes/models/users.js
+++ src/server/routes/models/users.js
@@ -2,13 +2,15 @@ import express from "express";
 import userModel from "../../models/Users";
 
 const router = express.Router();
+const userSchema = req('./Users');
 
 router.get("/:userId", async (req, res) => {
   // TODO
   // use params
   //  use functions in userModel with await
-  //   let result = await function goes here;
-  return res.status(200).json(result);
+	userSchema.isUserExist(req);	
+	const result = await userSchema.getUser(res.params.userId);
+  	return res.status(200).json(result);
 });
 
 router.post("/", (req, res) => {
@@ -16,15 +18,26 @@ router.post("/", (req, res) => {
   // body: name, email, type
   // passed in use as object
   //   let result = await function goes here;
-  return res.status(200).json(result);
+  /*	async (req, res) => {
+    		userSchema.isUserExist(req);
+		    return response.status(HttpStatus.OK).send({
+      		url: request.file.location
+    		});
+  	});
+  */
+	const result = await userSchema.createUser(req.body, req.params.userID);
+	return res.status(200).json(result);
 });
 
-router.delete("/:userId", (req, res) => {
+router.delete("/:userId", async (req, res) => {
   // TODO
   // use params
   //  use functions in userModel with await
-  //   let result = await function goes here;
-  return res.status(200).json(result);
+  //   let result = await function goes here
+
+	userSchema.isUserExist(req);
+  	let result = await userSchema.deleteUser(req.params.userId);
+  	return res.status(200).json(result);
 });
 
 module.exports = router;
