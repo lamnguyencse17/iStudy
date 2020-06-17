b8b8303f80be28afa9e005d84b002b411a0f5464
Nyan Dân <rrelita56@gmail.com>
2020-06-17 18:25:23 +0700

    Update users.js

--- src/server/routes/models/users.js
+++ src/server/routes/models/users.js
@@ -5,19 +5,13 @@ const router = express.Router();
 const userSchema = req('./Users');
 
 router.get("/:userId", async (req, res) => {
-  // TODO
-  // use params
-  //  use functions in userModel with await
+
 	userSchema.isUserExist(req);	
 	const result = await userSchema.getUser(res.params.userId);
   	return res.status(200).json(result);
 });
 
 router.post("/", (req, res) => {
-  // create new user
-  // body: name, email, type
-  // passed in use as object
-  //   let result = await function goes here;
   /*	async (req, res) => {
     		userSchema.isUserExist(req);
 		    return response.status(HttpStatus.OK).send({
@@ -30,11 +24,6 @@ router.post("/", (req, res) => {
 });
 
 router.delete("/:userId", async (req, res) => {
-  // TODO
-  // use params
-  //  use functions in userModel with await
-  //   let result = await function goes here
-
 	userSchema.isUserExist(req);
   	let result = await userSchema.deleteUser(req.params.userId);
   	return res.status(200).json(result);
