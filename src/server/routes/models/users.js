0b348216b26e64576b709755e89c4374fc75b17a
Nguyen Quang Lam <46278271+lamnguyencse17@users.noreply.github.com>
2020-06-19 00:55:55 +0700

    Fix problems mentioned in the issue

--- src/server/routes/models/users.js
+++ src/server/routes/models/users.js
@@ -6,25 +6,20 @@ const userSchema = req('./Users');
 
 router.get("/:userId", async (req, res) => {
 
-	userSchema.isUserExist(req);	
-	const result = await userSchema.getUser(res.params.userId);
+	let result = await userSchema.isUserExist(req);
+	if (result) {
+		return res.status(200).json(result)
+	}
+	result = await userSchema.getUser(res.params.userId);
   	return res.status(200).json(result);
 });
 
-router.post("/", (req, res) => {
-  /*	async (req, res) => {
-    		userSchema.isUserExist(req);
-		    return response.status(HttpStatus.OK).send({
-      		url: request.file.location
-    		});
-  	});
-  */
-	const result = await userSchema.createUser(req.body, req.params.userID);
+router.post("/", async (req, res) => {
+	let result = await userSchema.createUser(req.body);
 	return res.status(200).json(result);
 });
 
 router.delete("/:userId", async (req, res) => {
-	userSchema.isUserExist(req);
   	let result = await userSchema.deleteUser(req.params.userId);
   	return res.status(200).json(result);
 });
