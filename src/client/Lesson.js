d05a9e56f869bcd509809f22f363ab34ef4c3094
Nyan Dân <rrelita56@gmail.com>
2020-07-12 23:06:34 +0700

    Decorate Lesson.js And Its Components #20

--- src/client/Lesson.js
+++ src/client/Lesson.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Player from "./Lesson/Player";
+import NotePanel from "./Lesson/NotePanel";
+import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col'
 import axios from "axios";
 
 export default class Lesson extends Component {
@@ -36,10 +39,15 @@ export default class Lesson extends Component {
         {this.state.files == "" ? (
           <> </>
         ) : (
-          <>
-            <div className="h1">{this.state.title}</div>
-            <Player fileId={this.state.files} />
-          </>
+          <Container fluid>
+            <Col sm={8} md = {12}>
+              <div className="h1">{this.state.title}</div>
+              <Player fileId={this.state.files} />
+            </Col>
+            <Col sm={4} md = {6}>
+              <NotePanel />
+            </Col>
+          </Container>
         )}
       </>
     );
